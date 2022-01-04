71f6a6c0d5c9264f9b10988056a17692d2d993e4
OlehPalii <palij.oleg@gmail.com>
2022-01-04 16:23:14 +0000

    new

--- contract/tictactoe.cpp
+++ contract/tictactoe.cpp
@@ -1,49 +1,13 @@
 #include <eosio/eosio.hpp>
 
-// Message table
-struct [[eosio::table("message"), eosio::contract("talk")]] message {
-    uint64_t    id       = {}; // Non-0
-    uint64_t    reply_to = {}; // Non-0 if this is a reply
-    eosio::name user     = {};
-    std::string content  = {};
+using namespace eosio;
 
-    uint64_t primary_key() const { return id; }
-    uint64_t get_reply_to() const { return reply_to; }
-};
-
-using message_table = eosio::multi_index<
-    "message"_n, message, eosio::indexed_by<"by.reply.to"_n, eosio::const_mem_fun<message, uint64_t, &message::get_reply_to>>>;
-
-// The contract
-class talk : eosio::contract {
+class tictactoe : eosio::contract {
   public:
-    // Use contract's constructor
     using contract::contract;
 
-    // Post a message
-    [[eosio::action]] 
-    void post(uint64_t id, uint64_t reply_to, eosio::name user, const std::string& content) {
-        message_table table{get_self(), 0};
+    [[eosio::action]] void start( eosio::name host, eosio::name challenger) {
+        print("Welcome, challengers ", host," and ",challenger,"!");
 
-        // Check user
-        require_auth(user);
-
-        // Check reply_to exists
-        if (reply_to)
-            table.get(reply_to);
-
-        // Create an ID if user didn't specify one
-        //test 3
-        eosio::check(id < 1'000'000'000ull, "user-specified id is too large");
-        if (!id)
-            id = std::max(table.available_primary_key(), 1'000'000'000ull);
-
-        // Record the message
-        table.emplace(get_self(), [&](auto& message) {
-            message.id       = id;
-            message.reply_to = reply_to;
-            message.user     = user;
-            message.content  = content;
-        });
     }
 };
