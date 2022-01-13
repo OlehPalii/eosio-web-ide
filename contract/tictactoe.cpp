f07021d65595c5ea1db6285c4b8cf820160e74d7
OlehPalii <palij.oleg@gmail.com>
2022-01-13 11:46:38 +0000

    update

--- contract/tictactoe.cpp
+++ contract/tictactoe.cpp
@@ -1,13 +1,15 @@
 #include <eosio/eosio.hpp>
 
-using namespace eosio;
-
-class tictactoe : eosio::contract {
+class [[eosio::contract]] tictactoe : eosio::contract {
   public:
     using contract::contract;
 
     [[eosio::action]] void start( eosio::name host, eosio::name challenger) {
-        require_auth(get_self());
+        require_auth(host);
+        //require_auth(eosio::name("tictactoe"));
+        //require_auth(get_self());
+        //require_auth(get_self());
+        //check(has_auth(host) || has_auth(challenger), "User is not authorized to perform this action.");
         print("Welcome, challengers ", host," and ",challenger,"!");
     }
 };
